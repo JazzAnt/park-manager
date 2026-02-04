dd0d56bee67a43817d27b39b19a46a3a2fc38f0b
Jason A. Januar <jason.kwee98@gmail.com>
2026-02-04 01:14:22 -0800

    add facility to app for testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import Facility from "./components/Facility";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+let lorem = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum nisi ullam voluptatem accusantium officiis illo ducimus minima dolorum eum, recusandae incidunt iusto natus iste ut soluta ab at? Corporis, similique."
 
 function App() {
   return (
     <>
       <Header title="Park Management Site" subtitle="Manage Your Facilities Here"/>
+      <Facility name="Hello world!" description={lorem} imgSrc={reactLogo}/>
       <Footer />
     </>
   );
