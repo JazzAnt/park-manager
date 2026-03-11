44ddeff5fb5bf1f4bf751c22f981cc8a985f0238
Jason A. Januar <jason.kwee98@gmail.com>
2026-03-11 01:31:16 -0700

    add navbar css, also set root width to static (no more content wrap)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
-    <nav className="navigationBar">
+    <nav className="nav-bar">
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/add">Add Facility</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link className="nav-link" to="/">Home</Link></li>
+        <li><Link className="nav-link" to="/add">Add Facility</Link></li>
+        <li><Link className="nav-link" to="/about">About</Link></li>
       </ul>
     </nav>
   );
