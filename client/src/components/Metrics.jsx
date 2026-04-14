77b2bf8a21dfbcb036ed0a79a6924d326abb042f
Jason A. Januar <jason.kwee98@gmail.com>
2026-04-13 21:27:06 -0700

    update monthly profits metrics

--- client/src/components/Metrics.jsx
+++ client/src/components/Metrics.jsx
@@ -29,36 +29,100 @@ const Metrics = ({ facilities = [] }) => {
     return list.reduce((sum, facility) => {
       return sum + facility.profitsThisYear;
     }, 0);
-  }
+  };
 
   const getTotalCustomers = (list) => {
     return list.reduce((sum, facility) => {
       return sum + facility.customersThisYear;
     }, 0);
-  }
+  };
+
+  /**
+   * Creating an entire monthly profit database is out of the scope of this project.
+   * So this function simulates monthly profits by dividing the total profit by 12.
+   *
+   * By default each profit is evenly split to 1/12 of the profits. The optional
+   * multiplier attribute can be used to alter it. It's an array of 12 numbers
+   * which will be multiplicated to each 1/12 profit to change their distribution.
+   *
+   * For example the a multiplier of [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5]
+   * will halve the profits of the first 6 month and increase the profits of the latter 6 months by 50%.
+   * @param {*} list
+   * @param {*} multiplier
+   */
+  const generateMonthlyProfits = (
+    list,
+    multiplier = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ) => {
+    const yearlyProfit = getTotalProfits(list);
+    const avgMonthlyProfit = yearlyProfit / 12;
+    return multiplier.map((mult) => avgMonthlyProfit * mult);
+  };
 
   //TODO: Fetch data from database instead of static
-  let labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+  let labels = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   let monthly_profit = {
     labels: labels,
     datasets: [
       {
-        label: "Roller Coaster",
-        data: [100, 99, 102, 105, 110, 108, 113],
+        label: "Roller Coasters",
+        data: generateMonthlyProfits(
+          rollerCoasters,
+          [0.5, 0.6, 1.4, 1.2, 0.8, 0.9, 1.0, 0.9, 0.7, 1.5, 0.7, 1.8],
+        ),
         fill: false,
         borderColor: "red",
         tension: 0.1,
       },
       {
-        label: "Log Flume",
-        data: [30, 60, 55, 90, 85, 110, 120],
+        label: "Gentle Rides",
+        data: generateMonthlyProfits(
+          gentleRides,
+          [0.6, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 0.8, 0.6],
+        ),
+        fill: false,
+        borderColor: "orange",
+        tension: 0.1,
+      },
+      {
+        label: "Thrill Rides",
+        data: generateMonthlyProfits(
+          thrillRides,
+          [0.5, 0.5, 0.6, 1.8, 0.8, 1.6, 1.1, 1.0, 0.7, 2.0, 0.6, 0.8],
+        ),
+        fill: false,
+        borderColor: "purple",
+        tension: 0.1,
+      },
+      {
+        label: "Water Rides",
+        data: generateMonthlyProfits(
+          waterRides,
+          [0.4, 0.4, 0.7, 0.9, 1.2, 1.7, 2.0, 1.8, 0.8, 0.7, 0.6, 0.8],
+        ),
         fill: false,
         borderColor: "blue",
         tension: 0.1,
       },
       {
-        label: "Carousel",
-        data: [70, 55, 65, 45, 30, 40, 25],
+        label: "Food Stalls",
+        data: generateMonthlyProfits(
+          foodStalls,
+          [0.9, 0.9, 1.1, 1.1, 1.0, 1.0, 1.1, 1.1, 0.9, 1.0, 0.9, 1.0],
+        ),
         fill: false,
         borderColor: "green",
         tension: 0.1,
